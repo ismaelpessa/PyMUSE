c0b36bd47cb325b76a248994f9893bddad007bbb
ismaelpessa <ismael.pessa@gmail.com>
2017-07-06 13:37:03 -0400

    name

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 #from Cython.Build import cythonize
 
 setup(
-    name="Pymuse",
+    name="PyMUSE",
     version="0.1",
     description="Python software for handling VLT/MUSE data.",
     author="I. Pessa",
     license="BSD",
     author_email="ismael.pessa@gmail.com",
     url="https://github.com/ismaelpessa/Muse_Cube",
-    packages=['Pymuse'])
+    packages=['PyMUSE'])
