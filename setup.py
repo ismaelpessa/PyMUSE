64d114b26d7a32761c6ec6a84b9dc5392cacb85d
IsmaelPessa <18104503a>
2018-11-19 17:00:53 +0100

    version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="PyMUSE",
-    version="0.3.7",
+    version="0.3.8",
     description="Python software for handling VLT/MUSE data.",
     author="I. Pessa",
     license="MIT",
